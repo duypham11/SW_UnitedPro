3dbbf1fe6cc6866dcb019c884ba4c78c1745dc53
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2017-10-13 14:45:48 -0500

    no message

---
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/controller/Controller.java
@@ -0,0 +1,7 @@
+package org.unitedpro.mumsched.controller;
+
+/**
+ * Created by Duong Truong on 10/13/2017.
+ */
+public class Controller {
+}
---
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/dao/IDao.java
@@ -0,0 +1,7 @@
+package org.unitedpro.mumsched.dao;
+
+/**
+ * Created by Duong Truong on 10/13/2017.
+ */
+public interface IDao {
+}
