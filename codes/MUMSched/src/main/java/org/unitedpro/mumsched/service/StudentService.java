3791499f8b84fcd46d2b7bdf962ec59feb000e8a
duypham11 <duypham11@yahoo.com>
2017-10-15 10:23:44 -0500

    update

--- codes/MUMSched/src/main/java/org/unitedpro/mumsched/service/StudentService.java
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/service/StudentService.java
@@ -4,4 +4,6 @@ import org.unitedpro.mumsched.domain.*;
 public interface StudentService {
 	public void save(Student student);
 	public Student getStudentById(Long id);
+	public void delete(Student student);
+	public void update(Long id, Student student);
 }
--- codes/MUMSched/src/main/java/org/unitedpro/mumsched/serviceImpl/StudentServiceImpl.java
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/serviceImpl/StudentServiceImpl.java
@@ -2,11 +2,12 @@ package org.unitedpro.mumsched.serviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.unitedpro.mumsched.domain.*;
+import org.unitedpro.mumsched.service.StudentService;
 import org.unitedpro.mumsched.dao.*;
 
 
 @Service
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService{
 	@Autowired
 	StudentDAO studentDAO;
 	
@@ -17,5 +18,17 @@ public class StudentServiceImpl {
 
 	public Student getStudentById(Long studentId) {
 		return studentDAO.findStudentById(studentId);
+	}
+
+	@Override
+	public void delete(Student student) {
+		studentDAO.delete(student);
+		
+	}
+
+	@Override
+	public void update(Long id, Student student) {
+		studentDAO.findStudentById(id);
+		
 	}
 }
