1b24fd1a6dc89e4145a16d1035224e34bfec0d2a
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2017-10-16 10:05:08 -0500

    no message

---
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/controller/StudentController.java
@@ -0,0 +1,10 @@
+package org.unitedpro.mumsched.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Duong Truong on 10/16/2017.
+ */
+@Controller
+public class StudentController {
+}
