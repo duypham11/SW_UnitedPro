cf368e78dbb2e4428d4befa66ace90a21e8abc14
duypham11 <duypham11@yahoo.com>
2017-10-16 10:06:06 -0500

    update

--- codes/MUMSched/src/main/java/org/unitedpro/mumsched/MumSchedApplication.java
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/MumSchedApplication.java
@@ -2,10 +2,7 @@ package org.unitedpro.mumsched;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class MumSchedApplication extends SpringBootServletInitializer{
