315b3f62962bbb5e5d65f215e112daa723ad7a47
duypham11 <duypham11@yahoo.com>
2017-10-17 21:59:16 -0500

    up

--- codes/MUMSched/src/main/java/org/unitedpro/mumsched/MumSchedApplication.java
+++ codes/MUMSched/src/main/java/org/unitedpro/mumsched/MumSchedApplication.java
@@ -1,9 +1,5 @@
 package org.unitedpro.mumsched;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -12,7 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 public class MumSchedApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		//Persistence.createEntityManagerFactory("org.unitedpro.mumsched");
 		SpringApplication.run(MumSchedApplication.class, args);
 	}
 
